0b5d6034613661c16bfa887d4e7f43d0d460b692
guyInTheChair-8bit <reddyvenkatahemanth307@gmail.com>
2026-01-22 16:54:22 +0530

    Added code to use read to get input and to disable ECHO

--- main.c
+++ main.c
@@ -1,5 +1,33 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
+
+struct termios orig_termios;
+
+void enableRawMode();
+void disableRawMode();
 
 int main() {
-   printf("Helloo !!\n"); 
+    enableRawMode();
+    char c;
+    
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+
+    return 0;
+}
+
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+    
+    struct termios raw = orig_termios;
+    tcgetattr(STDIN_FILENO, &raw);
+
+    raw.c_lflag &= ~(ECHO);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
